458ed4e85dd84eec52cd5b9c0c254267075dc331
ashwithagadala <gadalaashwitha515@gmail.com>
2024-04-15 14:59:48 +0530

    first

---
+++ frontend/src/app/my-bids/my-bids.component.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+import { Column } from '../custom-table/columns';
+import { Bid } from 'src/models';
+import { BidService } from '../services/bid.service';
+
+@Component({
+  selector: 'app-my-bids',
+  templateUrl: './my-bids.component.html',
+  styleUrls: ['./my-bids.component.scss']
+})
+export class MyBidsComponent {
+
+  partyAddress : any;
+  bids : Bid[];
+
+  constructor(private bidService: BidService){
+  }
+
+  ngOnInit(): void {
+    this.partyAddress = localStorage.getItem("WALLETID");
+    console.log("my")
+    this.bidService.getMyBids(this.partyAddress).subscribe((bids) => {
+      bids.response.forEach(bid=>{
+        if(bid.Status=="0"){
+          bid.Status="PENDING"
+        }
+        else if(bid.Status=="1"){
+          bid.Status="REJECTED"
+        }
+        else{
+          bid.Status="APPROVED"
+        }
+      })
+        this.bids = bids.response;
+    });
+  }
+  tableColumns: Array<Column> =
+    [{ columnDef: 'Bid Clause', header: 'BidClause', cell: (element: Record<string, any>) => `${element['BidClause']}` },
+    { columnDef: 'Quote Amount', header: 'QuoteAmount', cell: (element: Record<string, any>) => `${element['QuoteAmount']}` },
+    { columnDef: 'Bid Status', header: 'Status', cell: (element: Record<string, any>) => `${element['Status']}` },
+    { columnDef: 'Actions', header: 'Actions', cell: (element: Record<string, any>) => `${element['Actions']}`, 
+        isActionsEnabled: true, 
+        tenderId: (element: Record<string, any>) => `${element['TenderId']}`, 
+        bidId: (element: Record<string, any>) => `${element['BidId']}`,
+        isDeleteEnabled: true, 
+        isEditEnabled: true, 
+        isViewTender: true}
+    ];
+
+  // tableData: Array<Bid> = [
+  //   { Status: "PENDING", "QuoteAmount": 23400, BidClause:"my bid" , BidId: 1, TenderId: 3 },
+  //   { Status: "PENDING", "QuoteAmount": 23400, BidClause:"my bid" , BidId: 1, TenderId: 7 },
+  // ];
+  
+}
